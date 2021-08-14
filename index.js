83291935bbe1499fecd8507d1edccd2644598a4e
Yazn <36912068+YaznSarah@users.noreply.github.com>
2021-08-14 14:19:41 -0700

    rebuilt the back-end using SQL server

--- index.js
+++ index.js
@@ -1,41 +1,66 @@
 const express = require('express');
 const cors = require('cors')
-const Database = require('mysql')
 const port = 3000;
 const app = express();
-let db;
+const mysql = require('promise-mysql');
 
-var con = Database.createConnection({
-    host: "localhost",
-    user: "admin",
-    password: "password"
-});
-
-con.connect((error) => {
-    if(error) throw error;
-    console.log("Connected!")
-    con.query("CREATE DATABASE mydb", (err, result) => {
-        if (err) throw err;
-        console.log("Database created");
+let con;
+(async () => {
+    con = await mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "simple",
+        database: "planner"
     });
-});
+})();
 
 app.use(cors());
 app.use(express.json());
 
 
-app.get('/tasks', (req, res) => {
-  res.json(req.body);
+app.get('/tasks', async (req, res) => {
+    const rows = await con.query("SELECT * FROM tasks");
+    res.json(rows);
 });
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
+    if(req.body.title == undefined){
+        return res.status(400).json({
+            "error": "title missing"
+        });
+    }
+    let sql = `INSERT INTO
+                    tasks
+                SET
+                    title = ?,
+                    status = ?,
+                    description = ?,
+                    date_created = NOW()`;
+    const values = [req.body.title, req.body.status, req.body.description];
+    await con.query(sql, values);
     res.json(req.body);
 });
 
 app.put('/tasks/:id', (req, res) => {
+    let sql = `UPDATE 
+                    tasks
+                SET 
+                    title = ?,
+                    status = ?,
+                    description = ?
+                WHERE
+                    id = ?`
+
+    const values = [req.body.title, req.body.status, req.body.description, req.params.id];
+    con.query(sql, values);
     res.json(req.body);
 });
 
 app.delete('/tasks/:id', (req, res) => {
+    let sql = `DELETE FROM
+                    tasks
+                WHERE id = ?`;
+    con.query(sql, [req.params.id]);
+    console.log(sql)
     res.json(req.body);
 });
 
